858c17b1d6e350003977532bf5649dbdaad378a7
Ross Whippo <rosswhippo@gmail.com>
2022-10-19 19:35:28 -0700

    Made nicer figures, added Reef Check urchin data from Dan

--- Cafeteria.R
+++ Cafeteria.R
@@ -59,6 +59,8 @@ consumed <- cafeteria %>%
   group_by(pycnoID, item) %>%
   summarise(eaten = sum(consumed))
 
+consumed$item <- factor(consumed$item, levels = c("green", "purple", "red", "mussel", "cucumber", "chiton"))
+
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # FIGURES                                                                      ####
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -70,12 +72,36 @@ consumed %>%
   geom_boxplot() +
   theme_minimal()
 
+# mean and sterror of consumption for urchins and mussels
+
+filter(consumed, item %in% c("green", "purple", "red", "mussel")) %>%
+ggplot(aes(x = item, y = eaten)) +
+  geom_jitter(col = "grey", size = 3, width = 0.15) +
+  scale_color_viridis(discrete = TRUE,
+                      begin = 0.3,
+                      end = 0.7,
+                      option = "magma") +
+  stat_summary(
+    geom = "point",
+    fun = "mean",
+    size = 6,
+    shape = 19
+  ) +
+  geom_errorbar(stat="summary", fun.data="mean_se", size = 1) +
+  theme_minimal() +
+  labs(x = "Prey Item", y = "Number Consumed") +
+  theme(axis.text.x = element_text(vjust = 5))
+
 # barplot of consumption by pycno
-consumed %>% filter(item %in% c('green', 'mussel', 'red', 'purple')) %>%
+consumed %>% filter(item %in% c('green', 'mussel', 'red', 'purple', 'cucumber')) %>%
   ggplot(aes(x = item, y = eaten, fill = item)) +
   geom_col() +
-  scale_fill_viridis(discrete = TRUE) +
-  facet_grid(pycnoID~.)
+  scale_fill_viridis(discrete = TRUE, option = 5, begin = 0.1, end = 0.8) +
+  facet_wrap(pycnoID~.) +
+  theme_bw() +
+  labs(x = "", y = "Number Consumed") +
+  theme(axis.text.x=element_blank(), axis.ticks.x = element_blank()) +
+  theme(legend.position = c(.9, .16))
 
 # barplot of prey sizes
 cafeteria %>%
@@ -90,11 +116,21 @@ cafeteria %>%
   geom_point(size = 2, position = "jitter") +
   scale_color_viridis(discrete = TRUE, begin = 0.2, end = 0.8, option = "D")
 
+
+consumed_stack <- consumed %>%
+  filter(item != "chiton") %>%
+  group_by(pycnoID, item) %>%
+  summarise(`Number Consumed` = sum(eaten))
+
 # total number of prey items eaten across all trials
-consumed %>%
-  ggplot(aes(x = item, y = eaten, fill = pycnoID)) +
-  geom_bar(stat = "sum") +
-  scale_fill_viridis(discrete = TRUE, option = "A")
+consumed_stack %>%
+  ggplot(aes(x = item, y = `Number Consumed`, fill = pycnoID)) +
+  geom_col() +
+  scale_fill_viridis(discrete = TRUE, option = 7) +
+  theme_minimal() +
+  labs(x = "Prey Item") +
+  theme(axis.text.x = element_text(vjust = 5), axis.text.y = element_text(hjust = 2)) +
+  guides(fill=guide_legend(title="Pycnopodia ID"))
 
 # Pycno Sizes
 PycnoMetrics %>%
@@ -110,3 +146,4 @@ PycnoMetrics %>%
 
 here <- cafeteria %>%
   mutate(consumed = as.integer(!(is.na(consumed_date))))
+
